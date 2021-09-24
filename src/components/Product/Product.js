9ac82cbfdef19ac61d97557144e7d8824f16cb79
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-09-24 12:01:35 +0600

    create search functionality and add dynamic rating icon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import "./Product.css"
+import Rating from 'react-rating';
 
 const Product = (props) => {
+    // console.log(props.product)
     const element = <FontAwesomeIcon icon={faShoppingCart} />
 
-    const { img, name, price, seller, stock } = props.product;
+    const { img, name, price, seller, stock, star } = props.product;
     return (
         <div className="product">
             <div className="product-image">
@@ -17,6 +19,13 @@ const Product = (props) => {
                 <p><small>by:{seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star rat-icon-color"
+                    fullSymbol="fas fa-star rat-icon-color "
+                    readonly>
+                </Rating>
+                <br />
                 <button onClick={() => props.handleAddToCart(props.product)} className="regular-button">{element} add to cart</button>
 
             </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,12 +6,17 @@ import "./Shop.css"
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
+
 
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data);
+                setDisplayProducts(data);
+            });
 
     }, []);
 
@@ -20,17 +25,30 @@ const Shop = () => {
         setCart(newCart);
     }
 
+    const handleSearch = event => {
+        const searchText = event.target.value;
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplayProducts(matchedProducts);
+    }
+
     return (
-        <div className='shop-container'>
-            <div className='product-container'>
-                {
-                    products.map(product => <Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>)
-                }
+        <>
+            <div className="search-container">
+                <input onChange={handleSearch} type="text" placeholder="type here to search" />
             </div>
-            <div className='shopping-cart'>
-                <Cart cart={cart}></Cart>
+
+            <div className='shop-container'>
+                <div className='product-container'>
+                    {
+                        displayProducts.map(product => <Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>)
+                    }
+                </div>
+                <div className='shopping-cart'>
+                    <Cart cart={cart}></Cart>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 };
 
